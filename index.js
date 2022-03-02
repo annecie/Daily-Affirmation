12de51d5677b0077e712baa8b9ab4e1085e7da5c
lreboseno22 <liamrebo22@gmail.com>
2022-03-02 18:29:04 -0500

    Fetch Giphy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const affirmationItem = document.getElementById("affirmation-item")
 const button = document.getElementById("button")
+const giphyDiv = document.getElementById("random-gif");
 
 // Fetch Api
 const requestOptions = {
@@ -13,7 +14,16 @@ const requestOptions = {
 button.addEventListener("click", () => {
     getAffirmation();
     displayAffirmation();
+    getGiphy();
 })
+
+const getGiphy = () => {
+  const url = "https://api.giphy.com/v1/stickers/random?api_key=lihApacEoXxEL3d4y1O8VBEWN8peBVcu&tag=positivity"
+  fetch(url)
+  .then(response => response.json())
+  .then(console.log)
+  .then(displayGiphy)
+}
 
 const getAffirmation = () => {
  fetch("https://www.affirmations.dev", requestOptions)
@@ -23,30 +33,32 @@ const getAffirmation = () => {
 }
 
 const displayAffirmation = (response) => {
-  // console.log(response.affirmation)
-  affirmationItem.innerText = response.affirmation
+  // affirmationItem.innerText = response.affirmation
 }
 
-const texts = document.querySelector(".text-bar");
+const displayGiphy = (data) => {
+  const giphy = document.createElement("img")
+}
+// const texts = document.querySelector(".text-bar");
 
-let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+// let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-const recognition = new SpeechRecognition();
-recognition.interimResults = true;
+// const recognition = new SpeechRecognition();
+// recognition.interimResults = true;
 
-let p = document.createElement("p");
+// let p = document.createElement("p");
 
-recognition.addEventListener("result", (e) => {
-  texts.appendChild(p);
-  const text = Array.from(e.results)
-    .map((result) => result[0])
-    .map((result) => result.transcript)
-    .join("");
-    p.innerText = text;
-});
-recognition.addEventListener("end", () => {
-    recognition.start();
-  });
+// recognition.addEventListener("result", (e) => {
+//   texts.appendChild(p);
+//   const text = Array.from(e.results)
+//     .map((result) => result[0])
+//     .map((result) => result.transcript)
+//     .join("");
+//     p.innerText = text;
+// });
+// recognition.addEventListener("end", () => {
+//     recognition.start();
+//   });
   
-  recognition.start();
+//   recognition.start();
 
